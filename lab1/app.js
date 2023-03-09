7cb0017dee7899e6c1cd4ad9aface56ff7ec10eb
unknown <ledung09102000fs@gmail.com>
2023-03-09 18:46:33 +0700

    xin 10 diem

---
+++ lab1/app.js
@@ -0,0 +1,13 @@
+// javascrips for lab1
+//khai bao bien a va nhap gia tri tu ban phim
+//nhap so nguyen a vao ban phim
+let a = promt("mời nhập vào số nguyên a:");
+let a1 = parseInt(a);
+//nhap so nguyen b vao ban phim
+let b = prompt("mời bạn nhập số nguyên b:");
+let b1 =parseInt(b);
+//tinh tong a va b
+let c = a1 + b1;
+//in ra ket qua bang 2 cach
+document.write("Tổng của" + a +"và" +b + "là :"+c);
+console.log(x);
