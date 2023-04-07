5d60b421fc2f2ce2aef4f79c32f86c80d9319f64
unknown <ledung09102000fs@gmail.com>
2023-04-07 09:27:47 +0700

    lab 5

---
+++ lab5/lab5.js
@@ -0,0 +1,17 @@
+//section
+let list =document.getElementById("task-list");
+let btnAdd = docunment.getElementById("btnAdd");
+
+btnAdd.
+//create name
+let name = document.createElement("input");
+name.type = "text";
+//create description
+let description = document.createElement("input");
+// create button delete 
+let btnDel = document.createElement("button");
+btnDel.innerHTML = "del";
+//create button edit
+let btnEdit = docunment.createElement("button");
+btnEdit.innerHTML = docunment.createElement("button");
+
